dfe5b9b6d30993f5637e4fd876a212888eb87dee
Minh Dinh <minh.dinhcong@gameloft.com>
2023-10-12 08:56:01 +0700

    Minor fix

--- BotManager.py
+++ BotManager.py
@@ -2,10 +2,16 @@ from operator import truediv
 from GitlabManager import GitlabManager
 from MattermostManger import MattermostManager
 
-GL_TOKEN = 'glpat-yuf_saDF2MVDUxwEhm3L'
+GL_TOKEN = 'glpat-d44ao3DZL-iLJX3hME_X'
 GL_HOST = 'https://git.gameloft.org/'
 PROJECT_ID = 5608
-MM_TESTCHANNEL = 'w5mb3pm65f8axe5e9prozkdhcc'
+MM_BUILDCHANNEL= 'zjx3u71su3g7ujabr5jerwdqqr'
+MM_TESTCHANNEL= 'w5mb3pm65f8axe5e9prozkdhcc'
+MM_DEFAULTCHANNEL = MM_BUILDCHANNEL
+MM_AUTHORID = '1znesrbpopgxtkmztocqr9tzuo' #minh.dinhcong
+MM_AUTHORIZEDUSER = ['minh.dinhcong@gameloft.com',  'dung.nguyenquoc3@gameloft.com', 'luan.nguyen@gameloft.com', 'hen.tranvan@gameloft.com', 'vien.phamphu@gameloft.com', 'nghia.tranhuu@gameloft.com']
+SAI_MEMBERS = ['lam.tranvan', 'luan.nguyen', 'minh.dinhcong', 'dung.nguyenquoc3', 'huy.buidang', 'loc.vovan', 'nghia.tranhuu', 'thach.nguyenngoc3', 'bao.nguyenngogia', 'thao.tranngan', 'anh.nguyenduy2','quang.phantri',
+               'hieu.nguyentrung10', 'phuc.vohuynhhoang', 'minh.phannhat', 'quan.luungoc', 'quan.letruong', 'kien.nguyenluong', 'hen.tranvan', 'vien.phamphu', 'hoang.vodinh', 'kien.lengoc', 'hieu.dotri2']
 
 class BotManager:
     def __init__(self):
@@ -15,12 +21,28 @@ class BotManager:
         self.mergeRequests = []
         self.Post = None
         self.iids = []
+        self.lastMessage = ''
+        self.AuthorizedUser = []
+        for email in MM_AUTHORIZEDUSER:
+            user = self.mmM.mmDriver.users.get_user_by_email(email)
+            #print('%s %s'%(email, user['id']))
+            self.AuthorizedUser.append(user['id'])
     
-    def GetAllMergeRequest(self,state='opened', author_usernames=[], target_branch=None):
-        for user in author_usernames:
-            self.mergeRequests+=self.project.mergerequests.list(state = state, author_username = user, target_branch=target_branch, iterator=True)
-            for mr in self.mergeRequests:
+    def GetAllMergeRequest(self,state='opened', target_branch=None):
+        post = self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': 'Bot Triggered! Getting merge requests...'})
+        if self.Post != None: 
+            self.mmM.mmDriver.posts.unpin_post_to_channel(self.Post['id'])
+            self.Post = None
+        self.mergeRequests = []
+        self.iids = []  
+        #self.mergeRequests+=self.project.mergerequests.list(state = state, author_username = user, target_branch=target_branch, iterator=True)
+        mrs = self.project.mergerequests.list(state = state, target_branch=target_branch, iterator=True)
+        for mr in mrs:
+            if mr.author['username'] in SAI_MEMBERS and mr.draft == False:
+                self.mergeRequests.append(mr)
                 self.iids.append(mr.iid)
+        self.mmM.mmDriver.posts.update_post(post['id'], options={'id': post['id'], 'channel_id': MM_DEFAULTCHANNEL, 'message': 'Merge requests query finished! Sending status...'})
+        self.SendMergeRequestStatusToMattermost()
 
     def ProcessMergeRequestToSend(self, merge_request):
         status = 'Opening'
@@ -36,42 +58,80 @@ class BotManager:
             else:
                 status = 'Can be merged'
                 icon = ':alert:'
-        return '\n|[%s](%s)|@%s|%s %s|'%(merge_request.title, merge_request.web_url, merge_request.author['username'],status, icon )
+        #return '\n|[%s](%s)|@%s|%s %s|'%(merge_request.title, merge_request.web_url, merge_request.author['username'],status, icon )
+        return '\n|[%s](%s)|[%s](https://chat.gameloft.org/sai/messages/@%s)|%s %s|'%(merge_request.title, merge_request.web_url, merge_request.author['username'],merge_request.author['username'],status, icon )
     
-    def SendMergeRequestStatusToMattermost(self, channel_id=MM_TESTCHANNEL):
+    def SendMergeRequestStatusToMattermost(self, channel_id=MM_DEFAULTCHANNEL):
+        if len(self.mergeRequests) < 1:
+            self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': 'No opened merge requests!'})
+            return -1
         res = '|merge request|author|status|\n|:-----|:-----|:------|'
         for mr in self.mergeRequests:
             res+=self.ProcessMergeRequestToSend(mr)
-        self.Post = self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_TESTCHANNEL, 'message': res})
+        self.Post = self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': res})#, 'root_id': self.TriggerThreadId
         self.mmM.mmDriver.posts.pin_post_to_channel(self.Post['id'])
-        print(self.Post)
     
-    def UpdateMergeRequestStatusInMattermost(self, channel_id=MM_TESTCHANNEL):
+    def UpdateMergeRequestStatusInMattermost(self, channel_id=MM_DEFAULTCHANNEL):
         res = '|merge request|author|status|\n|:-----|:-----|:------|'
         for mr in self.mergeRequests:
             res+=self.ProcessMergeRequestToSend(mr)
         if self.Post != None:
-            self.mmM.mmDriver.posts.update_post(self.Post['id'], options={'id': self.Post['id'], 'channel_id': MM_TESTCHANNEL, 'message': res, 'is_pinned': True})
+            self.mmM.mmDriver.posts.update_post(self.Post['id'], options={'id': self.Post['id'], 'channel_id': MM_DEFAULTCHANNEL, 'message': res, 'is_pinned': True})
     
-    def IsBotTriggered(self, channel_id='w5mb3pm65f8axe5e9prozkdhcc'):
+    def IsBotTriggered(self, channel_id=MM_DEFAULTCHANNEL):
         message = self.mmM.mmDriver.posts.get_posts_for_channel(channel_id=channel_id, params = {
             'page' : str(0),
             'per_page' : 1,
         })
         for id, value in message['posts'].items():
-            if(value['message'] == 'trigger_bot'):
-                if self.Post != None: 
-                    self.mmM.mmDriver.posts.unpin_post_to_channel(self.Post['id'])
-                    self.Post = None
-                    self.mergeRequests.clear()                
-                return True
-        return False
+            if self.lastMessage == value['message']:
+                self._SpamWarning()
+                return tuple(['update', 'none'])
+            message = value['message'].split(' ')
+            if message[0] == '!bot-linh' and value['user_id'] in self.AuthorizedUser and len(message) > 1:
+                if message[1].strip() == 'status':
+                    target_branch = 'none'
+                    if len(message) > 2:
+                        target_branch = message[2]
+                    return tuple(['status', target_branch])
+                elif message[1].strip() == 'reset':
+                    post = self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': 'Reseting...'})
+                    if self.Post != None: 
+                        self.mmM.mmDriver.posts.unpin_post_to_channel(self.Post['id'])
+                        self.Post = None
+                        self.mergeRequests = []
+                        self.iids = []  
+                    self.mmM.mmDriver.posts.update_post(post['id'], options={'id': post['id'], 'channel_id': MM_DEFAULTCHANNEL, 'message': 'Done!'})             
+                    return tuple(['reset', 'none'])  
+                elif message[1].strip() == 'off' and value['user_id'] == MM_AUTHORID:
+                    return tuple(['off', 'none'])
+                elif message[1].strip() == 'help':
+                    self._SendHelp()
+                    return tuple(['update', 'none'])
+                self.lastMessage = value['message']   
+            elif message[0].strip() == '!bot-linh' and value['user_id'] not in self.AuthorizedUser:
+                return tuple(['update', 'none'])
+        return tuple(['update', 'none'])
     
     def Update(self):
-        #loops = len(self.mergeRequests)
-        #for _ in range(loops):
-        #    self.mergeRequests+=[self.project.mergerequests.get(self.mergeRequests[0].iid)]
-        #    self.mergeRequests.pop(0)
         if len(self.iids) > 0:
             self.mergeRequests = self.project.mergerequests.list(iids=self.iids, iterator=True)
             self.UpdateMergeRequestStatusInMattermost()
+
+    def TurnedOffBot(self):
+        post = self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': 'Turning off...'})
+        if self.Post != None:
+            self.mmM.mmDriver.posts.unpin_post_to_channel(self.Post['id'])
+            self.Post = None
+            self.mergeRequests = []  
+            self.iids = []   
+        self.mmM.mmDriver.posts.update_post(post['id'], options={'id': post['id'], 'channel_id': MM_DEFAULTCHANNEL, 'message': 'Done!'})
+
+    def _SendHelp(self):
+        self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': '```!bot-linh status [target branch]```: get status\n```!bot-linh reset```: reset bot\n```!bot-linh help```: send help'})       
+
+    def _SpamWarning(self):
+        self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': 'No Spam!'})
+    
+    def _UnauthorizedUserWarning(self):
+        self.mmM.mmDriver.posts.create_post(options={'channel_id': MM_DEFAULTCHANNEL, 'message': 'No Permission!'})
--- main.py
+++ main.py
@@ -2,22 +2,27 @@
 #APEX team id: iy53wydmit8wtyyhis83gpni6o
 #SAI team id: s1pkgqt4xida3p6bo38kg9oa9c
 #SAI build channel: zjx3u71su3g7ujabr5jerwdqqr
+from turtle import update
 from BotManager import BotManager
 import datetime
 
+#MM_BUILDCHANNEL = 'zjx3u71su3g7ujabr5jerwdqqr'
 SAI_MEMBERS = ['lam.tranvan', 'luan.nguyen', 'minh.dinhcong', 'dung.nguyenquoc3', 'huy.buidang', 'loc.vovan', 'nghia.tranhuu', 'thach.nguyenngoc3', 'bao.nguyenngogia', 'thao.tranngan', 'anh.nguyenduy2','quang.phantri',
-               'hieu.nguyentrung10', 'phuc.vohuynhhoang', 'minh.phannhat', 'quan.luungoc', 'quan.letruong', 'kien.nguyenluong', 'hen.tranvan', 'vien.phamphu', 'arnau.font','julia.gasull']
+               'hieu.nguyentrung10', 'phuc.vohuynhhoang', 'minh.phannhat', 'quan.luungoc', 'quan.letruong', 'kien.nguyenluong', 'hen.tranvan', 'vien.phamphu', 'hoang.vodinh', 'kien.lengoc', 'hieu.dotri2']
 if __name__ == '__main__' :
     bot = BotManager()
     while(True):
-        if bot.IsBotTriggered() :
-            print('bot triggered')
-            mrs = bot.GetAllMergeRequest(author_usernames=SAI_MEMBERS, state='opened', target_branch='b_1.3.5a')
-            bot.SendMergeRequestStatusToMattermost()
-        bot.Update()
+        botResult = bot.IsBotTriggered()
+        if botResult[0] == 'status' and botResult[1] != 'none':
+            print('Getting status on branch %s'%(botResult[1]))
+            bot.GetAllMergeRequest(state='opened', target_branch=botResult[1])
+        elif botResult[0] in ['update', 'reset']:
+            bot.Update()
+        elif botResult[0] == 'off':
+            break
         print('%s waiting for signal...'%datetime.datetime.astimezone(datetime.datetime.now()))
-    #mrs = bot.GetAllMergeRequest(author_usernames=SAI_MEMBERS, target_branch='b_1.3.5a')
-    #bot.SendMergeRequestStatusToMattermost(merge_requests=mrs)
+    bot.TurnedOffBot()
+    print("Bot turned off")
             
 
 
